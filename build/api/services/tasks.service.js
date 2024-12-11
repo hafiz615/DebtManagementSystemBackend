bb1a6967cebce8b023ab62ea41701e965a63713b
Mohsin Tariq <moshin@hotprospector.com>
2024-12-11 18:23:53 +0500

    86b34vbhr: build

--- build/api/services/tasks.service.js
+++ build/api/services/tasks.service.js
@@ -10,6 +10,7 @@ const tasks_repomodel_1 = require("../../database/repomodels/tasks.repomodel");
 const case_util_1 = __importDefault(require("../../utils/case.util"));
 const case_repository_1 = require("../repository/case/case.repository");
 const common_util_1 = __importDefault(require("../../utils/common.util"));
+const email_util_1 = __importDefault(require("../../utils/email.util"));
 class TasksService {
     constructor() {
         this.tasksRepository = new tasks_repository_1.TasksRepository();
@@ -62,6 +63,7 @@ class TasksService {
         if (vaildatedTask.notes)
             history['Notes'] = vaildatedTask.notes;
         await case_util_1.default.addInHistory(history, caseId);
+        email_util_1.default.sendEmailOrSmsByEvent('case_task_added', caseId, null, reqTemp.id);
         return [true, task];
     }
     async updateTask(req) {
