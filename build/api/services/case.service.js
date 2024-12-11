80cab625fb91465a0ecfaf77ffe76421fa0f63ba
Mohsin Tariq <moshin@hotprospector.com>
2024-12-11 18:21:30 +0500

    86b34vfww: build

--- build/api/services/case.service.js
+++ build/api/services/case.service.js
@@ -616,6 +616,34 @@ class CaseService {
             const justifications = await case_util_1.default.getSettlementJustifications(caseTemp, models);
             return justifications;
         };
+        this.deleteFile = async (req) => {
+            let caseTemp = await this.caseRepository.getById(req.params.id, undefined, undefined, [{ path: 'debtor' }]);
+            if (!caseTemp) {
+                return [false, constants_util_1.default.notFoundMessage('case')];
+            }
+            // Extract the key from the request body
+            const { key } = req.body;
+            if (!key) {
+                return [false, "Key is not Found"];
+            }
+            const getDebtor = await this.debtorRepository.getOne({
+                _id: caseTemp.debtor,
+            });
+            if (!getDebtor) {
+                return [false, constants_util_1.default.notFoundMessage('debtor')];
+            }
+            // Filter the documents array to remove the document with the matching key
+            const updatedDocuments = getDebtor.documents.filter((doc) => doc.key !== key);
+            // If no document was removed, return a failure response
+            if (updatedDocuments.length === getDebtor.documents.length) {
+                return [false, `No document found with key: ${key}`];
+            }
+            // Update the debtor's documents with the filtered array
+            getDebtor.documents = updatedDocuments;
+            // Update the debtor in the database
+            const response = await this.debtorRepository.updateById(getDebtor._id, { documents: updatedDocuments });
+            return [true, `${key} is deleted successfully`];
+        };
         this.caseRepository = new case_repository_1.CaseRepository();
         this.uploadUtil = new upload_util_1.default();
         this.targetCFRepository = new targetCF_repository_1.TargetCFRepository();
