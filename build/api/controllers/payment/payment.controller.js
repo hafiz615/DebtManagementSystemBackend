7ce23572dc216e9dcf7db44345d4787359bf33c2
Ubaid Ur Rehman <ubaid477@gmail.com>
2024-12-15 21:11:03 +0500

    create build

--- build/api/controllers/payment/payment.controller.js
+++ build/api/controllers/payment/payment.controller.js
@@ -61,6 +61,31 @@ class PaymentController {
                     .send(responseHelper_util_1.default.get4xxResponse(constants_util_1.default.Messages.EXCEPTION));
             }
         };
+        this.getAllUpcomingPayments = async (req, res) => {
+            try {
+                const checkPermission = await common_util_1.default.checkPermission('viewCaseDetails', req);
+                if (!checkPermission)
+                    return res
+                        .status(constants_util_1.default.CODE.BAD_REQUEST)
+                        .send(responseHelper_util_1.default.get4xxResponse('You do not have permission to perform this operation'));
+                const response = await this.paymentService.getAllUpcomingPayments(req.params.id);
+                if (!response[0]) {
+                    return res
+                        .status(constants_util_1.default.CODE.OK)
+                        .send(responseHelper_util_1.default.get4xxResponse(response[1]));
+                }
+                return res.status(constants_util_1.default.CODE.OK).send(responseHelper_util_1.default.get2xxResponse({
+                    statusCode: constants_util_1.default.CODE.OK,
+                    data: response[1],
+                    message: constants_util_1.default.successFoundMessage('Case payments'),
+                }));
+            }
+            catch (error) {
+                return res
+                    .status(constants_util_1.default.CODE.BAD_REQUEST)
+                    .send(responseHelper_util_1.default.get4xxResponse(constants_util_1.default.Messages.EXCEPTION));
+            }
+        };
         this.addACHDetailsCreditor = async (req, res) => {
             try {
                 const response = await this.paymentService.addACHDetailsCreditor(req);
