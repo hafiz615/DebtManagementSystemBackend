714620f29794f2b2dbfb89052ed71ef346d21c17
Ubaid Ur Rehman <ubaid477@gmail.com>
2024-12-16 14:06:27 +0500

    Update payment status

--- build/api/services/debtor.service.js
+++ build/api/services/debtor.service.js
@@ -1244,6 +1244,7 @@ class DebtorService {
             let updatedPayment = await this.paymentRepository.updateById(transaction, {
                 authorized: 'Success', // Make is success so it can be picked up by CRON Job
                 captured: 'Success', // Make is success so it can be picked up by CRON Job
+                status: 'Pending',
                 dueDate: req.body.transactionDate,
                 debtorTransId: req.body.referenceId,
                 transactionType: req.body.transactionType,
