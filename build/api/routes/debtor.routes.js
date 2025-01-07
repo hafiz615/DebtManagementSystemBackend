0d171b99f3661204b1164698b9d93fbfc0fe04c5
Mohsin Tariq <moshin@hotprospector.com>
2025-01-07 17:08:07 +0500

    get-debtor-extracted-data

--- build/api/routes/debtor.routes.js
+++ build/api/routes/debtor.routes.js
@@ -46,5 +46,6 @@ router.put('/updateWeeklyBudget/:id', authorize_middleware_1.default.validateAut
 router.get('/getManualPayments/:id', authorize_middleware_1.default.validateAuth, debtor_controller_1.default.getManualPayments);
 router.post('/revertPayments/:id', authorize_middleware_1.default.validateAuth, debtor_validate_1.default.revertPayment, debtor_controller_1.default.revertPayments);
 router.post('/get-extracted-data', upload.array('files'), debtor_controller_1.default.getExtractFieldsAndDebtor);
+router.get('/get-debtor-extracted-data/:id', debtor_controller_1.default.getDebtorExtractedFields);
 exports.default = router;
 //# sourceMappingURL=debtor.routes.js.map
