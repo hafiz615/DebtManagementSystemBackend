0fcefbc0dfb3a69e9dee2d50b91abde6384c32a1
Mohsin Tariq <moshin@hotprospector.com>
2025-06-10 16:40:40 +0500

    emailThreading model changings

--- src/database/interfaces/emailThreading.interface.ts
+++ src/database/interfaces/emailThreading.interface.ts
@@ -3,6 +3,7 @@ import mongoose, {Document} from 'mongoose';
 export interface IEmailThreading extends Document {
   threadId: string;
   userId: mongoose.Schema.Types.ObjectId;
+  caseId: mongoose.Schema.Types.ObjectId;
   firstInboxMessage: mongoose.Schema.Types.ObjectId;
   previousMessages: mongoose.Schema.Types.ObjectId[];
   notificationStatus: boolean;
