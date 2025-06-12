0f1e719483b269f2efa65744b4e07f71b010a2bc
Mohsin Tariq <moshin@hotprospector.com>
2025-06-12 13:34:43 +0500

    partial

---
+++ src/api/repository/waterfall/waterfall.repository.interface.ts
@@ -0,0 +1,4 @@
+import {IWaterfall} from '../../../database/interfaces/waterfall.interface';
+import {IBaseRepository} from '../base.repository.interface';
+
+export interface IWaterfallRepository extends IBaseRepository<IWaterfall> {}
