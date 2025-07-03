f5d0d20fde7fa3933815678f37f94058bbd8bc74
Mohsin Tariq <moshin@hotprospector.com>
2025-07-03 15:33:07 +0500

    tylnex

--- build/utils/call.util.js
+++ build/utils/call.util.js
@@ -353,7 +353,7 @@ class CallUtil {
     async startTranscription(callControlId) {
         console.log(callControlId, 'callControlId');
         const data = {
-            ['transcription_engine']: 'A',
+            ['transcription_engine']: 'B',
             ['transcription_tracks']: 'both',
             ['command_id']: callControlId,
         };
--- src/utils/call.util.ts
+++ src/utils/call.util.ts
@@ -486,7 +486,7 @@ class CallUtil {
   async startTranscription(callControlId: string) {
     console.log(callControlId, 'callControlId');
     const data = {
-      ['transcription_engine']: 'A',
+      ['transcription_engine']: 'B',
       ['transcription_tracks']: 'both',
       ['command_id']: callControlId,
     };
