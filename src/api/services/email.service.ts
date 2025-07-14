499011a507cce53f38ef27e19cff39671571d6f0
Mohsin Tariq <moshin@hotprospector.com>
2025-07-14 12:48:54 +0500

    parse data

--- src/api/services/email.service.ts
+++ src/api/services/email.service.ts
@@ -78,8 +78,9 @@ class EmailService {
 
   async sendGridEmail(req: Request) {
     const reqTemp: any = req;
+    console.log('emaikl', req.body.email);
     const parseData = await simpleParser(req.body.email);
-    // console.log(parseData, 'okoko');
+    console.log(parseData, 'okoko');
     const subject = parseData.subject;
     const text = parseData.text;
     const from = parseData.from?.value[0].address;
